a7ac0666646e487e8d3bae23cbdbaa13e74a9339
ofek levi <ofeklevi36@gmail.com>
2024-03-22 19:40:44 +0200

    search fixed

--- src/services/filterData.js
+++ src/services/filterData.js
@@ -3,13 +3,16 @@ export const filterData = (data, searchValue, searchBy) => {
 
   const modifiedData = data?.pages.map((page) =>
     page.filter((data) => {
+      const { name, id, types } = data;
       if (!isSearching) return data;
 
-      if (searchBy === "id") return data.id.toString() === searchValue && data;
+      if (searchBy === "id") return id.toString() === searchValue && data;
 
-      if (searchBy === "name") return data.name.includes(searchValue) && data;
+      if (searchBy === "name")
+        return name.toLowerCase().includes(searchValue.toLowerCase()) && data;
 
-      if (searchBy === "type") return data.types.includes(searchValue) && data;
+      if (searchBy === "type")
+        return types.includes(searchValue.toLowerCase()) && data;
     })
   );
 
